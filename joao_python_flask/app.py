ced581367b1c7d76f2780f6500168e22db184da1
João Vitor Nunes Carvalho <161029294+joaozinho-cefet@users.noreply.github.com>
2024-08-16 08:38:22 -0300

    Add files via upload

--- joao_python_flask/app.py
+++ joao_python_flask/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'HASH-HASH-INVASOR_FDP@ablebleblebel_kshg**fiekn67gfsgfhVASHfvjÇShfvjsçfbJH'
 
 @app.route('/')
 def index():
@@ -12,6 +13,17 @@ def index():
 def aulinha(nome = 'João', curso = 'Informática', ano = 2):
     dados = {'nome':nome,'curso':curso, 'ano':ano}
     return render_template('aulasinha.html', dados_curso=dados)
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/dados',methods=['POST'])
+def dados():
+    flash('Dados enviados')
+    dados = request.form
+    return render_template('dados.html', dados=dados)
+
 
 if __name__ == '__main__':
     app.run()
