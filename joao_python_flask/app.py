1c3bee219fde71dc76263b1e198251e0e0d70d07
João Vitor Nunes Carvalho <161029294+joaozinho-cefet@users.noreply.github.com>
2024-08-23 08:46:38 -0300

    Add files via upload

--- joao_python_flask/app.py
+++ joao_python_flask/app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, request, flash
 app = Flask(__name__)
+from database import db
+from flask_migrate import Migrate
+from models import Usuario
 app.config['SECRET_KEY'] = 'HASH-HASH-INVASOR_FDP@ablebleblebel_kshg**fiekn67gfsgfhVASHfvjÇShfvjsçfbJH'
+
+
+# drive://usuario:senha@servidor/banco_de_dados
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/alunos"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
@@ -13,6 +25,13 @@ def index():
 def aulinha(nome = 'João', curso = 'Informática', ano = 2):
     dados = {'nome':nome,'curso':curso, 'ano':ano}
     return render_template('aulasinha.html', dados_curso=dados)
+
+@app.route('/usuario')
+def usuario():
+    u = Usuario.query.all()
+    return render_template('usuario_lista.html', dados = u)
+
+
 
 @app.route('/form')
 def form():
