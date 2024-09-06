7bed74f450b2f2d0bb3d6809706386566102d5a0
João Vitor Nunes Carvalho <161029294+joaozinho-cefet@users.noreply.github.com>
2024-09-06 08:39:01 -0300

    Add files via upload

--- joao_python_flask/app.py
+++ joao_python_flask/app.py
@@ -35,7 +35,7 @@ def projeto():
 def projeto_add():
     return render_template('usuario_add.html')
 
-@app.route('/projeto/save')
+@app.route('/projeto/save', methods=['POST'])
 def projeto_save():
     nome =request.form.get('nome')
     data_i =request.form.get('data_i')
@@ -49,6 +49,42 @@ def projeto_save():
     else:
         flash("Preencha Todos os Campos Corretamente!!!")
         return redirect('/projeto/add')
+
+@app.route('/projeto/remove/<int:id>')
+def projeto_remove(id):
+    projeto = Projeto.query.get(id)
+    if id > 0:
+        db.session.delete(projeto)
+        db.session.commit()
+        flash('usuario removido com suscesso!!!')
+        return redirect('/projeto')
+    else:
+        flash('Caminho incorreto!!!')
+        return redirect('/projeto')
+    
+@app.route('/projeto/edita/<int:id>')
+def projeto_edita(id):
+    projeto = Projeto.query.get(id)
+    return render_template('projeto_edita.html', dados = projeto)
+
+@app.route('/projeto/editasave', methods=['POST'])
+def projeto_editasave():
+     nome =request.form.get('nome')
+     data_i =request.form.get('data_i')
+     data_f =request.form.get('data_f')
+     id =request.form.get('id')
+     if id and nome and data_i and data_f:
+         projeto = Projeto.query.get(id)
+         projeto.nome = nome
+         projeto.data_i = data_i
+         projeto.data_f = data_f
+         db.session.commit()
+         flash("Dados atualizados!")
+         return redirect('/projeto')
+     else:
+         flash('Faltando dados')
+         return redirect('/projeto')
+         
 
 @app.route('/form')
 def form():
